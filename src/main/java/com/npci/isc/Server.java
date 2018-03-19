7e12d4a02468f870c2336f226d6f469d9dae11a8
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2018-03-19 16:38:07 +0330

    Change channel type to asccii channel

--- src/main/java/com/npci/isc/Server.java
+++ src/main/java/com/npci/isc/Server.java
@@ -3,6 +3,7 @@ package com.npci.isc;
 import com.npci.isc.packager.ShetabISO87APackager;
 import org.jpos.iso.ISOServer;
 import org.jpos.iso.ServerChannel;
+import org.jpos.iso.channel.ASCIIChannel;
 import org.jpos.iso.channel.XMLChannel;
 import org.jpos.util.LogSource;
 import org.jpos.util.Logger;
@@ -15,7 +16,7 @@ public class Server {
         try {
             Logger logger = new Logger();
             logger.addListener(new SimpleLogListener(System.out));
-            ServerChannel channel = new XMLChannel(new ShetabISO87APackager());
+            ServerChannel channel = new ASCIIChannel(new ShetabISO87APackager());
             ((LogSource) channel).setLogger(logger, "channel");
             ISOServer server = new ISOServer(8000, channel, null);
             server.setLogger(logger, "server");
